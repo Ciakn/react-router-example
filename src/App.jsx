c6cda6f54493468ddbe736234b1be9c1f26aff95
ciavash <svsh.kian@gmail.com>
2024-07-03 08:40:20 -0700

    creating navbar & layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import DashbordPage from './pages/DashbordPage';
+import PostPage from './pages/PostPage';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route>
-          <Route path="/" element={""} />
-          <Route path="/" element={""} />
-          <Route path="/" element={""} />
+        <Route path='' element={""}>
+          <Route path="/" index element={<HomePage />} />
+          <Route path="/" element={<DashbordPage />} />
+          <Route path="/" element={<PostPage/>} />
         </Route>
       </Routes>
     </>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  );
+}
+
+export default Navbar
