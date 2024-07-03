750792c5aa895c725f3f4f26f4470dd55c6237a0
ciavash <svsh.kian@gmail.com>
2024-07-03 08:58:55 -0700

    creating payment component

--- src/pages/DashbordPage.jsx
+++ src/pages/DashbordPage.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 function DashbordPage() {
   return (
-    <div>DashbordPage</div>
-  )
+    <div>
+      <div className="dashbord">
+        <div className="sidebar">
+          <NavLink to="payments">Payments</NavLink>
+          <NavLink to="profile">Profile</NavLink>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default DashbordPage
+export default DashbordPage;
