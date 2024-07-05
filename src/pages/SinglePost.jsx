37091c7cf6ebd5d2a431fa28650563feec5876b8
ciavash <svsh.kian@gmail.com>
2024-07-05 03:13:06 -0700

    developing single post details

--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-
+const posts = [
+  { id: 1, title: "post one", body: "bodyy one" },
+  { id: 2, title: "post two", body: "bodyy two" },
+  { id: 3, title: "post three", body: "bodyy three" },
+];
 function Post() {
   const { id } = useParams();
-  return <div>SinglePost {id}</div>;
+
+  const post = posts.find((p) => p.id == id);
+
+  return (
+    <div style={{backgroundColor :"gray" , borderRadius :"15px" , padding:"15px"}}>
+      <h2>post ID : {post.id}</h2>
+      <h4>post title : {post.title}</h4>
+      <p>body :{post.body}</p>
+    </div>
+  );
 }
 
 export default Post;
